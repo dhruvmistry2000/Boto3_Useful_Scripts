9dbf52f44ce9edd5057fbbb2d4ccf890d4d2aeff
dhruvmistry2000 <68422844+dhruvmistry2000@users.noreply.github.com>
2024-01-30 13:03:40 +0530

    Update S3_Create.py

--- S3_Create.py
+++ S3_Create.py
@@ -5,6 +5,6 @@ aws_management_console = boto3.session.Session(profile_name='default')
 s3_console = aws_management_console.client(service_name='s3', region_name='us-east-1')
 
 bucket_create = s3_console.create_bucket(
-        Bucket='dhruv-boto3-bucket-no1',
+        Bucket='Name_of_Your_Bucket',
 )
 print(bucket_create)
