69a425b261538f64a3c521917d5b87cf7dd306f7
dhruvmistry2000 <68422844+dhruvmistry2000@users.noreply.github.com>
2024-01-30 15:11:54 +0530

    Update S3_Delete.py

--- S3_Delete.py
+++ S3_Delete.py
@@ -5,7 +5,7 @@ bucket_name = input("Enter the S3 Bucket name = ")
 aws_management_console = boto3.session.Session(profile_name='default')
 s3_console = aws_management_console.client(service_name='s3', region_name='us-east-1')
 
-bucket_create = s3_console.delete_bucket(
+bucket_delete = s3_console.delete_bucket(
         Bucket=bucket_name
 )
-print(bucket_create)
+print(bucket_delete)
