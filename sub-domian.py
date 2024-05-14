75a779e709cf848b6c51b8d76d31c216060ab256
dhruvmistry2000 <dhruv.oneknotone@gmail.com>
2024-05-14 14:29:08 +0530

    Sub-domain script

---
+++ sub-domian.py
@@ -0,0 +1,47 @@
+import boto3
+
+def create_record_set(hosted_zone_id, sub_domain, instance_id):
+    client = boto3.client('route53')
+
+    # Get instance tags
+    ec2 = boto3.resource('ec2')
+    instance = ec2.Instance(instance_id)
+    tags = {tag['Key']: tag['Value'] for tag in instance.tags or []}
+
+    # Retrieve sub-domain name from tags
+    instance_sub_domain = tags.get('SubDomain')
+
+    if instance_sub_domain:
+        # Create a record set
+        change_batch = {
+            'Changes': [
+                {
+                    'Action': 'UPSERT',
+                    'ResourceRecordSet': {
+                        'Name': f"{instance_sub_domain}.{sub_domain}",
+                        'Type': 'A',
+                        'TTL': 300,  # TTL in seconds
+                        'ResourceRecords': [{'Value': instance.public_ip_address}]
+                    }
+                }
+            ]
+        }
+
+        # Submit the change batch
+        response = client.change_resource_record_sets(
+            HostedZoneId=hosted_zone_id,
+            ChangeBatch=change_batch
+        )
+
+        return response
+    else:
+        print("SubDomain tag not found. Cannot create record set.")
+
+# Example usage
+if __name__ == "__main__":
+    hosted_zone_id = "YOUR_HOSTED_ZONE_ID"
+    sub_domain = "example.com"
+    instance_id = "YOUR_INSTANCE_ID"
+
+    response = create_record_set(hosted_zone_id, sub_domain, instance_id)
+    print(response)
